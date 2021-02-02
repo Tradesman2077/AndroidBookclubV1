0859399da1def36af987b40d4a346751ef12ebb6
christopher wright <chriswright21@live.co.uk>
2021-02-02 11:30:50 +0000

    added more activities

--- app/src/main/java/com/example/compscilibrary/MainActivity.java
+++ app/src/main/java/com/example/compscilibrary/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.compscilibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button registerButton;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registerButton = findViewById(R.id.register_button);
+        loginButton = findViewById(R.id.login_button);
+
+        Intent registerIntent = new Intent(this, Register.class);
+        Intent loginIntent = new Intent(this, Login.class);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(loginIntent);
+            }
+        });
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(registerIntent);
+            }
+        });
+
     }
+
+
+
+
 }
+
