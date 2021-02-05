f545682c9c47ed976a79582d25e61be395f33b3d
christopher wright <chriswright21@live.co.uk>
2021-02-05 12:59:29 +0000

    finished login and register

--- app/src/main/java/com/example/compscilibrary/Login.java
+++ app/src/main/java/com/example/compscilibrary/Login.java
@@ -1,14 +1,92 @@
 package com.example.compscilibrary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class Login extends AppCompatActivity {
 
+    //Connection to firestore
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference users_db =
+            db.collection("CompSci_Db")
+                    .document("Users").collection("all_users");
+
+    //keys
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "password";
+    private static final String TAG = "activity_login";
+    public Button loginSubmit;
+    public EditText loginEmailInput;
+    public EditText loginPasswordInput;
+    private boolean alreadyMember = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginSubmit = findViewById(R.id.login_confirm_button);
+        loginEmailInput = findViewById(R.id.login_Email_Edit_Text);
+        loginPasswordInput = findViewById(R.id.login_password_edit_text);
+
+        Intent homeScreenIntent = new Intent(this, HomeScreen.class);
+
+        loginSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String userInputEmail = loginEmailInput.getText().toString();
+                String userInputPassword = loginPasswordInput.getText().toString();
+                //query db for user
+                Query query = users_db.whereEqualTo("email", userInputEmail);
+
+                query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                        if (task.isSuccessful()){
+                            for (QueryDocumentSnapshot document :task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                String password = (String) document.get(KEY_PASSWORD);
+                                Log.d(TAG, password + " "+ userInputPassword);
+
+                                if (userInputPassword.equals(password)){
+                                    startActivity(homeScreenIntent);
+                                    Toast.makeText(Login.this, "Welcome back", Toast.LENGTH_LONG).show();
+                                }
+                                else{
+                                    Toast.makeText(Login.this, "Password incorrect", Toast.LENGTH_LONG).show();
+                                }
+
+                            }
+                        }
+                        else {
+                            Log.d(TAG, "no user found: ", task.getException());
+                            alreadyMember=false;
+                            Toast.makeText(Login.this, "Account not found, please register", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/compscilibrary/Register.java
+++ app/src/main/java/com/example/compscilibrary/Register.java
@@ -7,17 +7,24 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,8 +38,8 @@ public class Register extends AppCompatActivity {
                     .document("Users").collection("all_users");
 
     //keys
-    public static final String KEY_TITLE = "email";
-    public static final String DESCRIPTION = "password";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "password";
     private EditText emailInput;
     private EditText passwordInput;
     private EditText passwordInputConfirm;
@@ -41,8 +48,7 @@ public class Register extends AppCompatActivity {
     private String userInputEmail;
     private String userPasswordInput;
     private String userPasswordConfirmInput;
-
-
+    private boolean alreadyMember;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,38 +61,79 @@ public class Register extends AppCompatActivity {
         confirmRegistrationButton = findViewById(R.id.submit_register_button);
         Intent loggedInIntent = new Intent(this, Login.class);
 
-
         confirmRegistrationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 //get user data from fields
-                userInputEmail = String.valueOf(emailInput.getText());
-                userPasswordInput = String.valueOf(passwordInput.getText());
-                userPasswordConfirmInput = String.valueOf(passwordInputConfirm.getText());
+                userInputEmail = emailInput.getText().toString();
+                userPasswordInput = passwordInput.getText().toString();
+                userPasswordConfirmInput = passwordInputConfirm.getText().toString();
 
-                //add to map
-                Map<String, Object> map = new HashMap<>();
-                map.put(KEY_TITLE, userInputEmail);
-                map.put(DESCRIPTION, userPasswordInput);
+                /* check if valid email entered before query */
 
-                //update db with map
-                users_db.add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Toast.makeText(Register.this, "You have been successfully registered", Toast.LENGTH_LONG).show();
+                if (isValidEmail(userInputEmail) && userPasswordInput.equals(userPasswordConfirmInput)){
+                    /*
+                check if user already registered
+                 query the email
+                */
+                    Query query = users_db.whereEqualTo("email", userInputEmail);
+                    query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                            if (task.isSuccessful()){
+                                for (QueryDocumentSnapshot document :task.getResult()) {
+                                    Log.d(TAG, document.getId() + " => " + document.getData());
+                                    alreadyMember=true;
+                                }
+                            }
+                            else {
+                                Log.d(TAG, "no user found: ", task.getException());
+                                alreadyMember=false;
+                            }
+                        }
+                    });
+
+                    //if not in db add and send to login activity or if already member send to login
+                    if (!alreadyMember){
+                        //add to map
+                        Map<String, Object> map = new HashMap<>();
+                        map.put(KEY_EMAIL, userInputEmail);
+                        map.put(KEY_PASSWORD, userPasswordInput);
+
+                        //update db with map of details
+                        users_db.add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Toast.makeText(Register.this, "You have been successfully registered", Toast.LENGTH_LONG).show();
+                                startActivity(loggedInIntent);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Register.this, "Something went wrong", Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    }
+                    else{
+                        Toast.makeText(Register.this, "You have already registered please log in", Toast.LENGTH_LONG).show();
                         startActivity(loggedInIntent);
-
                     }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(Register.this, "Something went wrong", Toast.LENGTH_LONG).show();
-                    }
-                });
-
+                }
+                else if(!userPasswordInput.equals(userPasswordConfirmInput)){
+                    Toast.makeText(Register.this, "Passwords don't match", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(Register.this, "Please enter a valid email", Toast.LENGTH_LONG).show();
+                }
             }
         });
+    }
+
+    //check if input is an email
+    public static boolean isValidEmail(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
     }
 
 }
